f2aa1b109f4733d3cd883a849bdcfe0eff547a0b
maxmudovismoi <urgaz123777@gmail.com>
2022-09-12 10:19:47 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+  document.getElementById("jsSidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("jsSidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
